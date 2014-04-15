adae0e0c5bf04841a557c3bdfa3b09c5dbb6ae76
Alex Bezuska <abezuska@gmail.com>
2014-04-15 16:03:30 -0400

    add meta content fix for small screens

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ $XMLTools = new XMLTools( array('XMLURL' => $XML) );
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title></title>
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 <style>
 
 </style>
